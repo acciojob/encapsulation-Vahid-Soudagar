4e5f5a1dc8bf7390b4957e2e833839742639de36
Vahid-Soudagar <mohammadvahidsoudagar264@gmail.com>
2023-11-18 12:11:47 +0530

    completed assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+
+        /*
+        D:\Accio\Spring1 Task\encapsulation-Vahid-Soudagar\src\main\java\com\driver\Main.java:6:12
+        java: name has private access in com.driver.RWOnly
+         */
+        obj.setName("Vahid");
+        System.out.println(obj.getName());
+    }
 }
